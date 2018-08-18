6b3918c0c394039bf97656c5edde0d61294b4456
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-08-18 16:24:29 -0700

    adding get for todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,35 +1,35 @@
-const expect = require('expect');
-const request = require('supertest');
+const expect = require('expect')
+const request = require('supertest')
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const {app} = require('./../server')
+const {Todo} = require('./../models/todo')
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => done())
 });
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
-    var text = 'Test todo text';
+    var text = 'Test todo text'
 
     request(app)
     .post('/todos')
     .send({text})
     .expect(200)
-    .expect( (res) => {
-        expect(res.body.text).toBe(text);
+    .expect((res) => {
+        expect(res.body.text).toBe(text)
     })
-    .end((err,res) => {
+    .end((err, res) => {
       if (err) {
-        return done(err);
+        return done(err)
       }
-      Todo.find().then( (todos) => {
-        expect(todos.length).toBe(1);
-        expect(todos[0].text).toBe(text);
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(1)
+        expect(todos[0].text).toBe(text)
         done();
-      }).catch ( (err) => done(err));
-    });
-  });
+      }).catch ( (err) => done(err))
+    })
+  })
 
   it('should not create a new todo with invalid body data', (done) => {
 
