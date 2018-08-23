9bbaf016e987bf282ae166258e2f038908fd54a9
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-08-22 20:38:43 -0700

    adding PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _=require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -72,6 +73,33 @@ app.delete('/todos/:id',(req, res) => {
     // console.log('error accessing to the db')
   });
 });
+
+app.patch('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+    // console.log('id is invalid');
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status.apply(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});  
+
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
