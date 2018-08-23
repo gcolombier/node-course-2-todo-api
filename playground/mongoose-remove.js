3d95548243aa5eeb5773828ed69b3d02e89681c9
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-08-22 21:39:10 -0700

    setup seperate test and development envs

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,15 @@
+const {ObjectId} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findByIdAndRemove({})
+// Todo.finOneAndRemove()
+
+Todo.findByIdAndRemove('5b7ce0df5e9d43575e57121a').then((todo) => {
+  console.log(todo);
+});
