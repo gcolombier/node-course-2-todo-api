c30045e1d108ad56828574ade9369bbaa4821470
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-08-26 21:20:32 -0700

    add get /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,26 @@ user.tokens = user.tokens.concat([{access,token}]);
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    return Promise.reject();
+    };
+
+  return User.findOne({
+    '_id':decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+
+};
+
 var User = mongoose.model('User', UserSchema);
 
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mangoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express();
 const port = process.env.PORT;
@@ -116,19 +117,11 @@ app.post('/users', (req, res) => {
   });
 });
 
-// // POST /users
-// app.post('/users', (req, res) => {
-//   var body = _.pick(req.body, ['email', 'password']);
-//   var user = new User(body);
 
-//   user.save().then(() => {
-//     return user.generateAuthToken();
-//   }).then((token) => {
-//     res.header('x-auth', token).send(user);
-//   }).catch((e) => {
-//     res.status(400).send(e);
-//   })
-// });
+
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user);
+});
 
 
 
