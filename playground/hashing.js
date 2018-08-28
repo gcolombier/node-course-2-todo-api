8385920297136bafb66a155f7184d33649f8d344
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-08-27 20:37:53 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,17 @@
-const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id:10
-}
+// const {SHA256} = require('crypto-js');
+// const jwt = require('jsonwebtoken');
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
+// var data = {
+//   id:10
+// }
 
-var decoded = jwt.verify(token,'123abc');
-console.log(decoded);
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'123abc');
+// console.log(decoded);
 // jwt.verify
 
 // var message = 'I am user number 3';
@@ -39,6 +41,18 @@ console.log(decoded);
 // }
 
 
+var password = '123abc!';
 
+// bcrypt.genSalt(10,(err,saltValue) => {
+//   bcrypt.hash(password,saltValue, (err,hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$eRBpdig.6ypSoNBaEQIwT.HVV.973897u5Mkct08oGN2kmohuzT3q';
+
+bcrypt.compare(password,hashedPassword,(err,result) => {
+  console.log(result);
+});
 
 
