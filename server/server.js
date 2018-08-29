5f969fbd0daba04c8a0a1490c5c7dcd4e7dedd54
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-08-28 21:28:58 -0700

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -109,7 +109,7 @@ app.post('/users', (req, res) => {
   var user = new User(body);
 
   user.save().then(() => {
-    return user.generateAuthToken();;
+    return user.generateAuthToken();
   }).then((token) => {
     res.header('x-auth',token).send(user)
   }).catch((e) => {
@@ -121,6 +121,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email,body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth',token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 
